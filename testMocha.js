9df6f0f8e156311210dbefb6fdf2fdf8515ead66
star1262 <j.sung16@gmail.com>
2019-08-06 16:50:03 +0900

    npm init and create mocha test file

---
+++ testMocha.js
@@ -0,0 +1,20 @@
+var assert = require("assert"); //nodejs에서 제공하는 aseert 모듈
+
+describe('Array 테스트', function() {
+	describe('indexOf() 메서드', function () {
+		it('값이 없을 때에는 -1 을 리턴함', function () {
+			assert.equal(-1, [1,2,3].indexOf(5));
+			assert.equal(-1, [1,2,3].indexOf(0));
+		});
+	});
+});
+
+describe('비동기 코드 테스트', function () {
+	describe('#setTimeout', function () {
+		it('2초 이내에 완료되지 않으면 실패', function (done) {
+			setTimeout(function () {
+				done();
+			}, 3000);
+		});
+	});
+});
